a86898dcd8808f412118686fd6845f1351bcb2ab
nelvson <internisti.jr@gmail.com>
2017-08-02 22:34:29 +0700

    Add submissionLinkParsing() ib create.py. Link parsing is now working for Kompas and Detik, tirto will catch up soon.

--- create.py
+++ create.py
@@ -1,22 +1,48 @@
 import time, nltk, sys
 from urllib import urlopen
 from bs4 import BeautifulSoup
+import re
 
 def printf(format, *args):
     sys.stdout.write(format % args)
 
 def submissionLinkParsing(zelda):
-    q = zelda
+    regex = ur"\.(.+?)\.+?"
+    link = re.findall(regex, zelda)
+    if (link==[]):
+        regex = ur"(.+?)\.+?"
+        link = re.findall(regex,zelda)
+    return link[0]
     # here goes catagorizing the link
 
 def commentBuilder(zelda):
     html = urlopen(zelda).read()
     param = BeautifulSoup(html, "html.parser")
     out=""
+
     for tag in param.find_all():
         out+=str(tag)
-    pos1 = out.find("detail_text")
-    pos2 = out.find("<!-- POLONG -->")
-    for x in range(pos1, pos2):
-        printf(out[x])
-    return out
+
+    comment=""
+
+    if (submissionLinkParsing(zelda)=='kompas'):
+        comment+=kompasCommentBuilder(out)
+    elif (submissionLinkParsing(zelda)=='detik'):
+        comment+=detikCommentBuilder(out)
+
+    return comment
+
+def kompasCommentBuilder(content):
+    #content is a string variable
+    #here goes kompasCommentBuilder
+    #content is always under  <div class="read__content"> tag
+
+def tirtoCommentBuilder(content):
+    #content is a string variable
+    #here goes tirtoCommentBuilder
+    #content is always under <div class="content-text-editor"> tag 
+
+def detikCommentBuilder(content):
+    #content is a string variable
+    #here goes detikCommentBuilder
+    #content is always under  <div class="detail_text" id="detikdetailtext"> tag
